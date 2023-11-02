efca0f2c292292924538b84bb57ecb09b147dac0
Bayron Ocampo <jafethocampo@icloud.com>
2023-11-02 01:13:30 -0600

    /public/img to /img

--- src/components/FilmsCard.jsx
+++ src/components/FilmsCard.jsx
@@ -6,7 +6,7 @@ const FilmsCard = ({title, epidoseID, director, img, url}) => {
   const navigate = useNavigate();
   return (
     <div className="film-card" onClick={() => navigate(url, {relative: 'path'})}>
-      <img src={`/public/img/films/${img+1}.jpg`} alt={`${title} movie poster`} />
+      <img src={`/img/films/${img+1}.jpg`} alt={`${title} movie poster`} />
       <h3>Star Wars: Epidose {epidoseID}  {title}</h3>
       <h4>Director: {director}</h4>
     </div>
